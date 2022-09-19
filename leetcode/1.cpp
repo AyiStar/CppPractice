de1c6f20a3f08b092e422ab82061ed2d3ebfdbfb
ayistar <ayistar@outlook.com>
2022-09-19 15:00:16 +0800

    basic config about cmake and catch2

---
+++ leetcode/1.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/two-sum/
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        size_t len = nums.size();
+        std::vector<int> result;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+TEST_CASE("EXAMPLE") {
+    std::vector<int> v {2, 7, 11, 15};
+    REQUIRE( Solution().twoSum(v, 9) == std::vector<int> {0, 1});
+}
