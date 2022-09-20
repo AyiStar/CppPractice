660709449425beddf707747fb47c1bc7524c3d4d
AyiStar <ayistar@outlook.com>
2022-09-20 19:40:55 +0800

    leetcode 22

---
+++ leetcode/22.cpp
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/generate-parentheses/
+
+#include <vector>
+#include <string>
+#include <stack>
+#include <catch2/catch_test_macros.hpp>
+
+class Solution {
+public:
+    std::vector<std::string> generateParenthesis(int n) {
+        std::vector<std::string> result;
+        std::vector<char> stk;
+        backTracking(2 * n, result, stk, 0);
+        return result;
+    }
+
+private:
+    void backTracking(const int n, std::vector<std::string> &result, std::vector<char> &stk, int status) {
+        if (stk.size() == n) {
+            if (status == 0) {
+                result.push_back(std::string(stk.begin(), stk.end()));
+            }
+            return;
+        }
+        // back track
+        stk.push_back('(');
+        backTracking(n, result, stk, status + 1);
+        stk.pop_back();
+        if (status > 0) {
+            stk.push_back(')');
+            backTracking(n, result, stk, status - 1);
+            stk.pop_back();
+        }
+    }    
+
+};
+
+TEST_CASE("EXAMPLE") {
+    const int n = 1;
+    const std::vector<std::string> result {
+        "()"
+    };
+    REQUIRE(Solution().generateParenthesis(n) == result);
+}
