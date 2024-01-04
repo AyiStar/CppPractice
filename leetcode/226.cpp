4f101dbabb649b5e7290db4ec859d65aa5d46f83
AyiStar <ayistar@outlook.com>
2024-01-04 21:26:41 +0800

    leetcode 226

---
+++ leetcode/226.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/invert-binary-tree
+#include <catch2/catch_test_macros.hpp>
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+        invertTree(root->left);
+        invertTree(root->right);
+        std::swap(root->left, root->right);
+        return root;
+    }
+};
+
+
+TEST_CASE("NO_TEST") {
+    
+}
