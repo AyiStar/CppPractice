d6439a7320150bcba346e78b6b98788bc33a3ad5
AyiStar <ayistar@outlook.com>
2024-02-17 21:06:26 +0800

    leetcode 131

--- leetcode/131.cpp
+++ leetcode/131.cpp
@@ -24,17 +24,13 @@ private:
     void backtrace(std::vector<int>& split_points, int i) {
         split_points.push_back(i);
         if (i == n_) {
-            for (std::size_t k = 0; k < split_points.size(); k++) {
-                std::cout << split_points[k] << " ";
-            }
-            std::cout << std::endl;
             results_.push_back(gen_split(split_points));
             split_points.pop_back();
             return;
         }
         
         for (int j = i + 1; j <= n_; j++) {
-            if (is_palindrome(sv_.substr(i, j))) {
+            if (is_palindrome(sv_.substr(i, j - i))) {
                 backtrace(split_points, j);
             }
         }
@@ -54,7 +50,7 @@ private:
     std::vector<std::string> gen_split(const std::vector<int>& split_points) {
         std::vector<std::string> v;
         for (int i = 0; i < static_cast<int>(split_points.size()) - 1; i++) {
-            auto substr = sv_.substr(split_points[i], split_points[i+1]);
+            auto substr = sv_.substr(split_points[i], split_points[i+1] - split_points[i]);
             v.push_back({substr.begin(), substr.end()});
         }
         return v;
